1d428f1c4390985a798204b06c61a85113c3e75a
Michał Bastrzyk <michal.bastrzyk.pl@gmail.com>
2025-03-25 19:36:17 +0100

    feat: Added prettier and prettier configuration

---
+++ prettier.config.mjs
@@ -0,0 +1,49 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+const config = {
+  endOfLine: "lf",
+  semi: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+
+  // Import sorting and division into groups
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^types$",
+    "^@/types/(.*)$",
+    "^@/lib/(.*)$",
+    "^@/hooks/(.*)$",
+    "^@/components/ui/(.*)$",
+    "^@/components/(.*)$",
+    "",
+    "^@/server/(.*)$",
+    "^@/trpc/(.*)$",
+    "^@/app/(.*)$",
+    "",
+    "^@/features/(.*)$",
+    "",
+    "^@/config/(.*)$",
+    "",
+    "^[./]",
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderBuiltinModulesToTop: true,
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true,
+
+  // TailwindCSS class sorting
+  tailwindFunctions: ["cva", "cn"],
+  tailwindStylesheet: "./src/styles/globals.css",
+}
+
+export default config
