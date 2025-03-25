3e58e31da11251fa23a609b922489722860b48d7
Michał Bastrzyk <michal.bastrzyk.pl@gmail.com>
2025-03-25 20:05:42 +0100

    feat: Refactor global styles and implement custom font configuration

---
+++ src/config/fonts.ts
@@ -0,0 +1,11 @@
+import { Source_Code_Pro, Source_Sans_3 } from "next/font/google"
+
+export const sans = Source_Sans_3({
+  variable: "--font-sans",
+  subsets: ["latin"],
+})
+
+export const mono = Source_Code_Pro({
+  variable: "--font-mono",
+  subsets: ["latin"],
+})
