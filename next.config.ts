f359200d44410194e4ea106fd9ed96f716acbf60
Michał Bastrzyk <michal.bastrzyk.pl@gmail.com>
2025-03-25 20:19:21 +0100

    feat: Add environment variable validation with @t3-oss/env-nextjs and zod

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next"
 
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "./src/env.js"
+
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+
   experimental: {
     inlineCss: true,
   },
