83ef550006ea7db5e7daee12cf924950745ccbbd
Michał Bastrzyk <michal.bastrzyk.pl@gmail.com>
2025-03-28 00:02:33 +0100

    style: update sidebar variant padding in shell component for improved layout

--- src/components/shell.tsx
+++ src/components/shell.tsx
@@ -8,7 +8,7 @@ export const shellVariants = cva("grid items-center gap-8 pb-8 pt-6 md:py-8", {
   variants: {
     variant: {
       default: "container",
-      sidebar: "",
+      sidebar: "px-6",
       centered: "container flex h-dvh max-w-2xl flex-col justify-center py-16",
       markdown: "container max-w-3xl py-8 md:py-10 lg:py-10",
     },
