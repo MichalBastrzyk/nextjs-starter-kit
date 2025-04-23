bcf7c1efda807f1ff845d87ce41e9a79f7c392ef
Michał Bastrzyk <michal.bastrzyk.pl@gmail.com>
2025-04-23 17:06:07 +0200

    feat: Introduce AuthLayout component and simplify authentication pages by removing redundant layout code

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,29 +1,12 @@
 import * as React from "react"
-import Link from "next/link"
 
 import { Skeleton } from "@/components/ui/skeleton"
 import { ResetPasswordForm } from "@/components/auth/reset-password-form"
-import { Icons } from "@/components/icons"
-
-import { siteConfig } from "@/config/site"
 
 export default function ResetPassword() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <Link
-          href="/"
-          className="flex items-center gap-2 self-center font-medium"
-        >
-          <div className="bg-primary text-primary-foreground flex h-6 w-6 items-center justify-center rounded-md">
-            <Icons.logo className="size-4" />
-          </div>
-          {siteConfig.title}
-        </Link>
-        <React.Suspense fallback={<Skeleton className="h-[300px] w-full" />}>
-          <ResetPasswordForm />
-        </React.Suspense>
-      </div>
-    </div>
+    <React.Suspense fallback={<Skeleton className="h-[300px] w-full" />}>
+      <ResetPasswordForm />
+    </React.Suspense>
   )
 }
