70fa596278c2f98d752edb2583d66321c7ab59b0
Michał Bastrzyk <michal.bastrzyk.pl@gmail.com>
2025-05-05 16:39:14 +0200

    feat: add Stripe customer creation and webhook listener to user authentication

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,7 +12,9 @@ export default async function DashboardMainPage() {
   return (
     <div className="flex h-[200vh] flex-1 flex-col">
       <div className="@container/main flex flex-1 flex-col gap-2">
-        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6"></div>
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+          <pre>{JSON.stringify(auth.user.stripeCustomerId, null, 2)}</pre>
+        </div>
       </div>
     </div>
   )
